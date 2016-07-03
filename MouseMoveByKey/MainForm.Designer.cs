4e96d423450f001dcef3af05ad2ef7fa0b4a4eef
Jason Wells <jason@wells.solutions>
2016-07-03 00:45:45 -0500

    Prototype Complete

---
+++ MouseMoveByKey/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace MouseMoveByKey {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.sliderSensitivity = new System.Windows.Forms.TrackBar();
+            this.lblSensitivity = new System.Windows.Forms.Label();
+            this.chkActivateLeftRight = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderSensitivity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sliderSensitivity
+            // 
+            this.sliderSensitivity.Enabled = false;
+            this.sliderSensitivity.LargeChange = 15;
+            this.sliderSensitivity.Location = new System.Drawing.Point(85, 146);
+            this.sliderSensitivity.Maximum = 50;
+            this.sliderSensitivity.Minimum = 1;
+            this.sliderSensitivity.Name = "sliderSensitivity";
+            this.sliderSensitivity.Size = new System.Drawing.Size(401, 56);
+            this.sliderSensitivity.TabIndex = 0;
+            this.sliderSensitivity.Value = 20;
+            this.sliderSensitivity.Scroll += new System.EventHandler(this.sliderSensitivity_Scroll);
+            // 
+            // lblSensitivity
+            // 
+            this.lblSensitivity.AutoSize = true;
+            this.lblSensitivity.Location = new System.Drawing.Point(85, 123);
+            this.lblSensitivity.Name = "lblSensitivity";
+            this.lblSensitivity.Size = new System.Drawing.Size(399, 17);
+            this.lblSensitivity.TabIndex = 1;
+            this.lblSensitivity.Text = "Mouse-Move Sensitivity (De-activate above to enable change)";
+            // 
+            // chkActivateLeftRight
+            // 
+            this.chkActivateLeftRight.AutoSize = true;
+            this.chkActivateLeftRight.Checked = true;
+            this.chkActivateLeftRight.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkActivateLeftRight.Location = new System.Drawing.Point(85, 49);
+            this.chkActivateLeftRight.Name = "chkActivateLeftRight";
+            this.chkActivateLeftRight.Size = new System.Drawing.Size(365, 21);
+            this.chkActivateLeftRight.TabIndex = 2;
+            this.chkActivateLeftRight.Text = "Activate Mouse-Move by Arrow Keys and PgUp/PgDn";
+            this.chkActivateLeftRight.UseVisualStyleBackColor = true;
+            this.chkActivateLeftRight.CheckedChanged += new System.EventHandler(this.chkActivateLeftRight_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 226);
+            this.Controls.Add(this.chkActivateLeftRight);
+            this.Controls.Add(this.lblSensitivity);
+            this.Controls.Add(this.sliderSensitivity);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MouseMove-By-Key";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sliderSensitivity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar sliderSensitivity;
+        private System.Windows.Forms.Label lblSensitivity;
+        private System.Windows.Forms.CheckBox chkActivateLeftRight;
+    }
+}
+
---
+++ MouseMoveByKey/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MouseMoveByKey {
+    public partial class MainForm : Form {
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const uint MOUSEEVENTF_MOVE = 0x0001;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private Timer checkKeyDown = null;
+        
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private static bool _leftPressed = false;
+        private static bool _rightPressed = false;
+        private static bool _upPressed = false;
+        private static bool _downPressed = false;
+        private int _moveFactor = 2;
+        
+        private void MainForm_Load(object sender, EventArgs e) {
+            //setup low level keyboard hook to detect keypresses
+            _hookID = SetHook(_proc);
+
+            //start thread to check key-down flags
+            checkKeyDown = new Timer();
+            checkKeyDown.Interval = 5; //millisecond resolution for checking keydown state
+            checkKeyDown.Tick += CheckKeyDown_Tick;
+            checkKeyDown.Start();
+        }
+
+        private void CheckKeyDown_Tick(object sender, EventArgs e) {
+            if(chkActivateLeftRight.Checked) {
+                if(_leftPressed) {
+                    mouse_event(MOUSEEVENTF_MOVE, -1 * _moveFactor * sliderSensitivity.Value, 0, 0, (UIntPtr)0);
+                } else if(_rightPressed) {
+                    mouse_event(MOUSEEVENTF_MOVE, 1 * _moveFactor * sliderSensitivity.Value, 0, 0, (UIntPtr)0);
+                } else if(_upPressed) {
+                    mouse_event(MOUSEEVENTF_MOVE, 0, -1 * _moveFactor * sliderSensitivity.Value, 0, (UIntPtr)0);
+                } else if(_downPressed) {
+                    mouse_event(MOUSEEVENTF_MOVE, 0, 1 * _moveFactor * sliderSensitivity.Value, 0, (UIntPtr)0);
+                }
+            }
+        }
+        
+        private void chkActivateLeftRight_CheckedChanged(object sender, EventArgs e) {
+            sliderSensitivity.Enabled = (!chkActivateLeftRight.Checked);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e) {
+            if(checkKeyDown != null)
+                checkKeyDown.Dispose();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+            using(Process curProcess = Process.GetCurrentProcess()) {
+                using(ProcessModule curModule = curProcess.MainModule) {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            if(nCode >= 0) {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if(wParam == (IntPtr)WM_KEYDOWN) {
+                    _leftPressed = ((Keys)vkCode == Keys.Left);
+                    _rightPressed = ((Keys)vkCode == Keys.Right);
+                    _upPressed = ((Keys)vkCode == Keys.PageUp);
+                    _downPressed = ((Keys)vkCode == Keys.PageDown);
+                } else if(wParam == (IntPtr)WM_KEYUP) {
+                    if((Keys)vkCode == Keys.Left) {
+                        _leftPressed = false;
+                    } else if((Keys)vkCode == Keys.Right) {
+                        _rightPressed = false;
+                    } else if((Keys)vkCode == Keys.PageUp) {
+                        _upPressed = false;
+                    } else if((Keys)vkCode == Keys.PageDown) {
+                        _downPressed = false;
+                    }
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private void sliderSensitivity_Scroll(object sender, EventArgs e) {
+
+        }
+    }
+}
